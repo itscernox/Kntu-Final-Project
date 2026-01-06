1fefb4b35cbfa1ff60bf21adb5ff469adc1a17ba
Mahyar Mousavi <cernox@atomicmail.io>
2026-01-06 09:48:41 +0330

    Transform data from old-sheet to new one after reallocation + Minor adjustments

--- Test/main.c
+++ Test/main.c
@@ -1,3 +1,5 @@
+//Next steps : set value & set formula function || cell info func (print value & type of vlaue & formula condition & ...)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -7,14 +9,15 @@ struct  CELL_INFO{
     long long int int_num;
     int int_set;
     double float_num;
-    int float_set;
+    int float_set;   
+    char formula[100];
+    int formula_set;    
 
     /*
         status show what kind of data does the sell have stored
         0 : None
         1 : int 
         2 : float 
-        3 : double 
     */
    
     int status[1];
@@ -23,10 +26,8 @@ struct  CELL_INFO{
 void identify_cells(struct CELL_INFO * , int , int);
 void display_cells(struct CELL_INFO * , int  , int );
 void check_operator(char [] , int *  , int * , struct CELL_INFO **);
+void resize_sheet(struct CELL_INFO ** , int * , int *);
 void help();
-void sheet_settings(struct CELL_INFO ** , int * , int *);
-//void Data_Transformation(struct CELL_INFO * , int , int , int , int);
-
 
 int main(){
 
@@ -50,14 +51,16 @@ int main(){
 
     display_cells(cells , rows , cols);
 
-    // set cells status to not filled at start
+    //Fill cells with 0 at start & update cells info
     int index;
     for (int r = 0 ; r < rows ; r++){
         for (int c = 0 ; c < cols ; c++){
             index = r * cols + c;
-            cells[index].int_set = 0;
+            cells[index].int_num = 0;
+            cells[index].int_set = 1;
             cells[index].float_set = 0;
-            cells[index].status[0] = 0;
+            cells[index].formula_set = 0;
+            cells[index].status[0] = 1;
         }
     }
 
@@ -88,8 +91,8 @@ void check_operator(char opt[] , int * rows , int * cols , struct CELL_INFO **ce
         help();
     }
 
-    else if (strcmp(opt , "sheet settings") == 0 || strcmp(opt , "ss") == 0){
-        sheet_settings(cells , rows , cols);
+    else if (strcmp(opt , "resize sheet") == 0 || strcmp(opt , "rs") == 0){
+        resize_sheet(cells , rows , cols);
     }
 
     else if (strcmp(opt , "show cells") == 0 || strcmp(opt , "sc") == 0){
@@ -101,7 +104,7 @@ void check_operator(char opt[] , int * rows , int * cols , struct CELL_INFO **ce
         printf("\n      Your Input is not valid!\n");
 }
 
-void sheet_settings(struct CELL_INFO **cells , int *rows , int *cols){
+void resize_sheet(struct CELL_INFO **cells , int *rows , int *cols){
     int r = *rows;
     int c = *cols;
     printf("\n      Enter New Rows : "); scanf("%d" , rows);
@@ -123,30 +126,109 @@ void sheet_settings(struct CELL_INFO **cells , int *rows , int *cols){
             return;
         } 
     }
+    
+    //make a copy of old sheet
+    //struct CELL_INFO *old_cells = *cells;
+    struct CELL_INFO *old_cells = (struct CELL_INFO *) malloc (r * c * sizeof(struct CELL_INFO));
+    if(!old_cells){
+        printf("Backup allocation failed!\n");
+        return;
+    }
+    memcpy(old_cells , *cells , r * c * sizeof(struct CELL_INFO));
 
+    //realloc cells in another pointer to avoid missing data when allocation failure happend
     struct CELL_INFO *tmp = realloc(*cells, (*rows) * (*cols) * sizeof(struct CELL_INFO));
 
     if (!tmp) {
         printf("\nRealloc failed! Keeping old sheet.\n");
         *rows = r;
         *cols = c;
+        free(old_cells);
         return;
     }
 
-    *cells = tmp;  
+    //Transforming datas from old sheet to new one
+    int index_0;
+    int index_1;
+    int a = (r < *rows)? r : *rows;
+    int b = (c < *cols)? c : *cols;
+    int arr[a*b];
+    int i = 0;
+    for (int x = 0 ; x < a ; x++){
+        for (int y = 0 ; y < b ; y++){
+            // index of old cells
+            index_0 = x * c + y;
+            
+            // equivalant index in new cells(temp)
+            index_1 = x * (*cols) + y;
 
+            arr[i] = index_1;
+            i++;
+
+            /*
+            if (old_cells[index_0].int_set == 1){
+                tmp[index_1].int_num = old_cells[index_0].int_num;
+            }
+
+            else if (old_cells[index_0].float_set == 1){
+                tmp[index_1].float_num = old_cells[index_0].float_num;
+            }
+
+            if (old_cells[index_0].formula_set == 1){
+                strncpy(tmp[index_1].formula, old_cells[index_0].formula, sizeof(tmp[index_1].formula));
+                tmp[index_1].formula[sizeof(tmp[index_1].formula) - 1] = '\0';
+            }
+
+            tmp[index_1].int_set = old_cells[index_0].int_set;
+            tmp[index_1].float_set = old_cells[index_0].float_set;
+            tmp[index_1].formula_set = old_cells[index_0].formula_set;
+            tmp[index_1].status[0] = old_cells[index_0].status[0];
+            */
+
+
+            // the line below do same as the upper commented code because struct contains only fixed-size arrays
+            // copy the entire cell
+            tmp[index_1] = old_cells[index_0];
+        }
+    }
+
+    free(old_cells);
+    
+    //update extra cells(fill with 0 & ...) if sheet has been extended
+    int index;
+    if (*rows > r || *cols > c){
+        for(int i = 0 ; i < *rows ; i++){
+            for (int j = 0 ; j < *cols ; j++){
+                index = i * (*cols) + j;
+                int found = 0;
+                for(int k = 0 ; k < a * b ; k++)
+                    if(arr[k] == index){
+                        found = 1;
+                        break;
+                    }
+
+                if (found == 0){
+                    tmp[index].int_num = 0;
+                    tmp[index].int_set = 1;
+                    tmp[index].float_set = 0;
+                    tmp[index].formula_set = 0;
+                    tmp[index].status[0] = 1;
+                }
+            }
+        }
+    }
+
+    *cells = tmp;  
     identify_cells(*cells, *rows, *cols);    
 }
 
-// void Data_Transformation()
-
 void help(){
     printf("\n      list of operators you can use : \n");
-    printf("\n      1 - sheet settings (or ss as short command)");
-    printf("\n          by using this command you can increase or reduce num of rows and columns");
-    printf("\n          BECAREFUL : reducing rows or cols may delete your datas from sheet\n");
-    printf("\n      2 - show cells (or sc as short command)");
-    printf("\n          by using this command you can see current sheet setting\n");
+    printf("\n      command 1 - resize sheet (or rs as short command)");
+    printf("\n                  by using this command you can increase or reduce num of rows and columns");
+    printf("\n                  BECAREFUL : reducing rows or cols may delete your datas from sheet\n");
+    printf("\n      command 2 - show cells (or sc as short command)");
+    printf("\n                  by using this command you can see current sheet pattern\n");
 
 
 }
